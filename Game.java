7c9d206679429bd853c1cba49535ce459efafee1
inatrox <mniltop@googlemail.com>
2024-01-18 13:01:03 +0100

    Added Game.java a and TextManager.java

--- Game.java
+++
@@ -1,9 +0,0 @@
-public class Game {
-    public static void main(String[] args){
-        TextManager text_manager = new TextManager();
-        String test = "Halloooooooooooooooooooooooooo";
-
-        text_manager.description(test);
-
-    }
-}
--- TextManager.java
+++
@@ -1,8 +0,0 @@
-
-// this class describes how text is displayed on the console
-// it should not be used to store text on this class
-public class TextManager {
-    public void description(String description_text){
-        System.out.print(description_text);
-    }
-}
