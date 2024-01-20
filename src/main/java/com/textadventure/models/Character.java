39bdb28228a39b3faa9c8e7298829b45bd328034
inatrox <mniltop@googlemail.com>
2024-01-20 23:55:56 +0100

    Added Player Classes

--- src/main/java/com/textadventure/models/Character.java
+++ src/main/java/com/textadventure/models/Character.java
@@ -1,8 +1,63 @@
 package src.main.java.com.textadventure.models;
 
 // An abstract base class for all characters in the game, including player characters and non-player characters (NPCs).
-
 public class Character
 {
+    private int health;
+    private int strength;
+    private int intelligence;
+    private int endurance;
+    private int speed;
 
+    public Character() {}
+
+    public void setHealth(int healthValue)
+    {
+        this.health = healthValue;
+    }
+
+    public void setStrength(int stregthValue)
+    {
+        this.strength = stregthValue;
+    }
+
+    public void setIntelligence(int intelligenceValue)
+    {
+        this.intelligence = intelligenceValue;
+    }
+
+    public void setEndurance(int enduranceValue)
+    {
+        endurance = enduranceValue;
+    }
+
+    public void setSpeed(int speedValue)
+    {
+        speed = speedValue;
+    }
+
+    public int getHealth()
+    {
+        return this.health;
+    }
+
+    public int getStrength()
+    {
+        return this.strength;
+    }
+
+    public int getIntelligence()
+    {
+        return intelligence;
+    }
+
+    public int getEndurance()
+    {
+        return endurance;
+    }
+
+    public int getSpeed()
+    {
+        return speed;
+    }
 }
--- src/main/java/com/textadventure/models/Player.java
+++ src/main/java/com/textadventure/models/Player.java
@@ -1,4 +1,35 @@
 package src.main.java.com.textadventure.models;// A class that inherits from Character and includes specific functions and properties for the player.
 
-public class Player {
+public class Player extends Character{
+    enum PlayerClasses
+    {
+        FIGHTER,
+        MAGE,
+        PALADIN,
+        ROGUE
+    }
+
+    private final int fighterInitialHealth = 10;
+    private final int fighterInitialStrength = 10;
+    private final int fighterInitialIntelligence = 10;
+    private final int fighterInitialEndurance = 10;
+    private final int fighterInitialSpeed = 10;
+
+    private final int mageInitialHealth = 10;
+    private final int mageInitialStrength = 10;
+    private final int mageInitialIntelligence = 10;
+    private final int mageInitialEndurance = 10;
+    private final int mageInitialSpeed = 10;
+
+    private final int paladinInitialHealth = 10;
+    private final int paladinInitialStrength = 10;
+    private final int paladinInitialIntelligence = 10;
+    private final int paladinInitialEndurance = 10;
+    private final int paladinInitialSpeed = 10;
+
+    private final int rougeInitialHealth = 10;
+    private final int rougeInitialStrength = 10;
+    private final int rougeInitialIntelligence = 10;
+    private final int rougeInitialEndurance = 10;
+    private final int rougeInitialSpeed = 10;
 }
